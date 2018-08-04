e79e444433ec86daf768ca1699971643cd17afc9
Chris <chrispackett1998@gmail.com>
2018-08-04 18:02:53 -0400

    created the createDeck func to make array of card objects, now shuffle

--- public/main.js
+++ public/main.js
@@ -1,5 +1,22 @@
-const createDeckOfCards = () => {
-  console.log("Hey there, we're going to create that deck of cards you asked for!")
+let deck = []
+let userHand = []
+let dealerHand = []
+
+const createDeck = () => {
+  let suits = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
+  let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+  let rank_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+  for (let s = 0; s < suits.length; s++) {
+    for (let r = 0; r < ranks.length; r++) {
+      const card = {
+        suit: suits[s],
+        rank: ranks[r],
+        value: rank_values[r]
+      }
+      deck.push(card)
+    }
+  }
+  console.log(deck)
 }
 
-document.addEventListener('DOMContentLoaded', createDeckOfCards)
+document.addEventListener('DOMContentLoaded', createDeck)
